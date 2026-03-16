ba50196c238fa63249734fe49ced1a6bd0a5df4d
pjlkaw <pedrorochabispo@gmail.com>
2026-03-16 17:49:28 -0300

    sistema de módulos

---
+++ sistema de modulos/Sistema de módulos.js
@@ -0,0 +1,47 @@
+import { soma} from "./math.js"
+// const readlinesync = require("readline-sync")
+import readlinesync from "readline-sync"
+import chalk from "chalk"
+
+const valor1 = parseInt(readlinesync.question("Valor 1:"))
+const valor2 = parseInt(readlinesync.question("Valor 2:"))
+
+console.log(chalk.bgCyan(" - " +
+    soma(valor1,valor2) +
+" - "));
+ 
+
+
+
+
+
+// [3] SISTEMA DE MÓDULOS
+// Arquivos:
+// math.js
+// sistema_de_modulos.js
+
+// Objetivo:
+// Aprender import e export.
+
+// Ideia:
+// Criar funções matemáticas em outro arquivo.
+
+// Fluxo:
+// math.js cria funções
+// ↓
+// exporta funções
+// ↓
+// arquivo principal importa
+// ↓
+// usa as funções
+
+// Passos:
+// - criar função somar()
+// - exportar função
+// - importar no arquivo principal
+// - executar função
+
+// Conceitos:
+// import
+// export
+// organização de código
---
+++ sistema de modulos/math.js
@@ -0,0 +1,3 @@
+export function soma(valor1, valor2) {
+    return valor1 + valor2
+}
