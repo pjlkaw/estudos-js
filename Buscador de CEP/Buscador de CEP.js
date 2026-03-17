617f71900b6933a9d5d12f87c0023c8e1b1da53e
pjlkaw <pedrorochabispo@gmail.com>
2026-03-17 13:48:16 -0300

    Buscador de cep finalizado - 4/9

--- Buscador de CEP/Buscador de CEP.js
+++ Buscador de CEP/Buscador de CEP.js
@@ -3,20 +3,32 @@ import readlineSync from "readline-sync"
 async function buscarDados() {
     
     try {
-        const cep = readlineSync.question("Insira um CEP para buscar: ")
-        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        const data = await response.json()
-        console.log(response)
-        console.log(
-            `
-            | CEP: ${data.cep}
-            | Logradouro: ${data.logradouro}
-            | Bairro: ${data.bairro}
-            | Localidade: ${data.localidade}
-            | UF: ${data.uf}
-            | Estado: ${data.estado}
-            `
-        )
+        let cep = readlineSync.question("Insira um CEP para buscar: ")
+        cep = cep.replace(/\D/g, "")
+        // console.log(response)
+        if(cep.length !== 8){
+            console.log("CEP não encontrado, tente novamente ou digite outro CEP..: \n");
+            return buscarDados()
+        }
+        else {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            const data = await response.json()
+            if(data.erro){
+                console.log("CEP não encontrado, tente novamente ou digite outro CEP..: \n");
+                return buscarDados()
+            }
+            console.log(
+                `
+                | CEP: ${data.cep}
+                | Logradouro: ${data.logradouro}
+                | Bairro: ${data.bairro}
+                | Localidade: ${data.localidade}
+                | UF: ${data.uf}
+                | Estado: ${data.estado}
+                `
+            )            
+        }
+
     }
     catch(error) {
         console.log(error);
