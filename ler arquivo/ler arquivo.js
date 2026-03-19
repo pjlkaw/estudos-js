26d826fd4ff778e4d721fefac1c63bb90a50cb33
pjlkaw <pedrorochabispo@gmail.com>
2026-03-19 16:50:36 -0300

    validação S/N para abrir o arquivo

--- ler arquivo/ler arquivo.js
+++ ler arquivo/ler arquivo.js
@@ -1,12 +1,34 @@
 import fs from "fs"
 import readlineSync from "readline-sync"
+import { exec } from "child_process"
 
-const arquivo = readlineSync.questionInt('Número do arquivo para ser alterado: ')
-const input = readlineSync.question(`Substitua o conteúdo de mensagem${arquivo}.txt com: `)
-fs.writeFileSync(`mensagem${arquivo}.txt`, input)
-const conteudo = fs.readFileSync(`mensagem${arquivo}.txt`, "utf-8")
+console.log(`Arquivos da pasta:`)
+const pasta = fs.readdirSync(".") //pega arquivos da pasta em forma de array
+
+//tratamento da pasta para aparecer apenas arquivos .txt
+const pastaFiltrada = pasta.filter(arquivo=>arquivo.endsWith(".txt")) //sem a condição do forEach ele segue o programa sem filtrar e mostrando todos arquivos da pasta
+pastaFiltrada.forEach((arquivo, index) => {
+    console.log(`${index} - ${arquivo}`);
+});
+
+
+const indice = readlineSync.questionInt('Número do arquivo para ser alterado: ')
+const arquivoEscolhido = pastaFiltrada[indice]
+const input = readlineSync.question(`Substitua o conteúdo de ${arquivoEscolhido} com: `)
+
+
+fs.writeFileSync(arquivoEscolhido, input)
+const conteudo = fs.readFileSync(arquivoEscolhido, "utf-8")
 console.log("Conteúdo alterado para:")
 console.log(conteudo);
+
+
+const verArquivo = readlineSync.question('Deseja abrir o arquivo? (S/N) ')
+if (verArquivo == "S" || "s") {
+    exec(`xdg-open ${arquivoEscolhido}`) //"xdg-open" exclusivo para linux
+} else {
+    console.log("Fim do Programa");
+}
  
 
 // [5] LER ARQUIVO
