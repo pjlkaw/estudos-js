703db9704185dd74259ca713d978eaf9c1b3c7c6
pjlkaw <pedrorochabispo@gmail.com>
2026-03-23 00:10:10 -0300

    Gerador de senhas - Finalizado 6/9

--- gerador de senha/geradorSenha.js
+++ gerador de senha/geradorSenha.js
@@ -1,38 +1,33 @@
-import { log } from 'console';
 import fs from 'fs'
-// import inquirer from 'inquirer'; inquirer não funcionando no sistema da escola, fazer 2 versões usando o readline e inquirer
 import readlineSync from 'readline-sync'
+
+console.log("=========================");
+console.log("=== GERADOR DE SENHAS ===");
+console.log("=========================");
+
 
 const tamanhoSenha = readlineSync.questionInt('Digite o tamanho da senha: ')
 if (tamanhoSenha > 100) {
     console.log("Senha grande de mais para executar")
-    return
-}
-const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
-let senha = ''
-for(let i = 0; i<tamanhoSenha; i++) {
-    let caracterAleatorio = Math.floor(Math.random() * caracteres.length)
-    senha += caracteres[caracterAleatorio]
-}
-console.log("Senha gerada!\n" + senha)
-
-const salvarSenhaInput = readlineSync.question(`\nDeseja salvar a senha ' ${senha} '? (S/N)`)
-if(salvarSenhaInput == "S" || salvarSenhaInput == "s") {
-    fs.appendFileSync("senhas.txt", senha + "\n")
-    console.log("Senha salva no arquivo senhas.txt")
 }
 else {
-    console.log("Fim do Programa");  
+    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+    let senha = ''
+    for(let i = 0; i<tamanhoSenha; i++) {
+        let caracterAleatorio = Math.floor(Math.random() * caracteres.length)
+        senha += caracteres[caracterAleatorio]
+    }
+    console.log("Senha gerada!\n" + senha)
+    const salvarSenhaInput = readlineSync.question(`\nDeseja salvar a senha ' ${senha} '? (S/N)`)
+    if(salvarSenhaInput == "S" || salvarSenhaInput == "s") {
+        fs.appendFileSync("senhas.txt", senha + "\n")
+        console.log("Senha salva no arquivo senhas.txt")
+    }
+    else {
+        console.log("Fim do Programa!");  
+    }
 }
 
-
-// inquirer.prompt([
-//     {
-//         type: 'list',
-//         message: `O que você gostaria de fazer com a senha ${senha}`,
-//         choices: 'salvar no arquivo senhas.txt'
-//     }
-// ])
 
 // [6] GERADOR DE SENHA
 // Arquivo: gerador_de_senha.js
