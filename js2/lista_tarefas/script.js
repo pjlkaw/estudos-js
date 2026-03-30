78553feec51c9d050001a6c49df8c60e22be3793
pjlkaw <pedrorochabispo@gmail.com>
2026-03-30 01:30:40 -0300

    Lista Tarefas - 1/9

--- js2/lista_tarefas/script.js
+++ js2/lista_tarefas/script.js
@@ -1,4 +1,23 @@
+let id = 1
+document.getElementById('addBtn').addEventListener('click', () => {
 
+    const input = document.getElementById('input').value; console.log(input);
+    const table = document.getElementById('table')    
+    const conteudotarefa = document.createElement('div')
+    conteudotarefa.innerHTML = `
+        <div id="tarefa">
+            <p>${id++}</p>
+            <p>${input}</p>
+            <button class="deleteBtn">Delete</button>
+        </div>
+    ` 
+
+    table.appendChild(conteudotarefa)
+    
+    conteudotarefa.querySelector('.deleteBtn').addEventListener('click', () => {
+        conteudotarefa.remove()
+    })
+})
 
 
 
