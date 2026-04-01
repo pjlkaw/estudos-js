346862c963b61ea057fa9d82e72553f96a26f5b3
pjlkaw <pedrorochabispo@gmail.com>
2026-04-01 20:25:15 -0300

    HTML simples consumindo API

---
+++ js2/apiFront/apifront.js
@@ -0,0 +1,54 @@
+document.getElementById('btn').addEventListener('click', () => {
+    fetch('https://brasilapi.com.br/api/ibge/municipios/v1/SP?providers=dados-abertos-br,gov,wikipedia')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data[1].nome);
+        const p = document.getElementById('p')
+        p.textContent = data[1].nome
+
+        const input = document.getElementById('input').value
+
+        const parentDiv = document.getElementById('parent')
+        const novop = document.createElement('p')
+        novop.textContent = `${data[input].nome} ---- ${data[input].codigo_ibge}`
+        parentDiv.appendChild(novop)
+    })
+})
+
+
+
+
+
+
+
+
+
+// [3] CONSUMINDO API (FRONT)
+// Arquivo: api_front.js
+
+// Objetivo:
+// Aprender a buscar dados de APIs.
+
+// Ideia:
+// Buscar uma frase ou imagem de API externa.
+
+// Fluxo:
+// botão clicado
+// ↓
+// fetch()
+// ↓
+// resposta JSON
+// ↓
+// mostrar na tela
+
+// Passos:
+
+// * criar botão
+// * usar fetch()
+// * usar await response.json()
+// * mostrar resultado no HTML
+
+// Conceitos:
+// fetch
+// async/await
+// requisição HTTP
