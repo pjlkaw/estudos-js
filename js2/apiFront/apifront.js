7f04a6220fbfaccdbf7c8c70775068ffd5f21c59
pjlkaw <pedrorochabispo@gmail.com>
2026-04-01 20:48:54 -0300

    Requisição JSON simplificada com await e try

--- js2/apiFront/apifront.js
+++ js2/apiFront/apifront.js
@@ -1,22 +1,44 @@
-document.getElementById('btn').addEventListener('click', () => {
-    fetch('https://brasilapi.com.br/api/ibge/municipios/v1/SP?providers=dados-abertos-br,gov,wikipedia')
-    .then(res => res.json())
-    .then(data => {
+
+
+// document.getElementById('btn').addEventListener('click', () => {
+//     fetch('https://brasilapi.com.br/api/ibge/municipios/v1/SP?providers=dados-abertos-br,gov,wikipedia')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data[1].nome);
+//         const p = document.getElementById('p')
+//         p.textContent = data[1].nome
+
+//         const input = document.getElementById('input').value
+
+//         const parentDiv = document.getElementById('parent')
+//         const novop = document.createElement('p')
+//         novop.textContent = `${data[input].nome} ---- ${data[input].codigo_ibge}`
+//         parentDiv.appendChild(novop)
+//     })
+// })
+
+
+document.getElementById('btn').addEventListener('click', async function() {
+    try {
+        //Requisição do JSON simplificada
+        const response = await fetch('https://brasilapi.com.br/api/ibge/municipios/v1/SP?providers=dados-abertos-br,gov,wikipedia')
+        const data = await response.json()
+
+        //Exibição no HTML
         console.log(data[1].nome);
         const p = document.getElementById('p')
         p.textContent = data[1].nome
-
+        
         const input = document.getElementById('input').value
-
         const parentDiv = document.getElementById('parent')
         const novop = document.createElement('p')
         novop.textContent = `${data[input].nome} ---- ${data[input].codigo_ibge}`
         parentDiv.appendChild(novop)
-    })
+    }
+    catch (error){
+        console.log(error);
+    }
 })
-
-
-
 
 
 
